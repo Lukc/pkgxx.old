9a9c8e65ff5246968b82648905b8979814343ede
MrPantoufle <mrpantoufle@upyum.com>
2010-11-27 14:00:43 +0100

    E_BUILD-like errors are now #defines.

--- src/error.sh.h
+++ src/error.sh.h
@@ -1,21 +1,20 @@
 
 /* Pkgfile and general errors */
-E_GENERAL=1
-E_PKGFILE=2
-E_DIR_PERM=3
-E_DOWNLOAD=4
-E_UNPACK=5
-E_CONTROL_SUM=6
-E_FOOTPRINT=7
-E_BUILD=8
-E_INSTALL=9
+#define E_GENERAL 1
+#define E_PKGFILE 2
+#define E_DIR_PERM 3
+#define E_DOWNLOAD 4
+#define E_UNPACK 5
+#define E_CONTROL_SUM 6
+#define E_FOOTPRINT 7
+#define E_BUILD 8
+#define E_INSTALL 9
 
-/* Invalid choices */
-E_INVALID_DOWNLOAD_TOOL=10
-E_INVALID_MAN_COMPRESSION=11
-E_INVALID_PM=12 /* PM == Package Manager */
+/*  Invalid choices */
+#define E_INVALID_DOWNLOAD_TOOL 10
+#define E_INVALID_MAN_COMPRESSION 11
+#define E_INVALID_PM 12 /* PM == Package Manager */
 
-/* Unbuildable package */
-E_BAD_ARCH=20
-E_BAD_KERNEL=21
-
+/*  Inbuildable package */
+#define E_BAD_ARCH 20
+#define E_BAD_KERNEL 21
