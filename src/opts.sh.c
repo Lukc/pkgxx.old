e384c1b44b1751aa283ceeda010e9611152f4229
Lukc <lukc@upyum.com>
2010-11-28 07:46:36 +0100

    Not working -pm switch added.  - The long option is --package-manager  - It currently does not work, probably because of a problem of variable    exportation or localisation.

--- src/opts.sh.c
+++ src/opts.sh.c
@@ -2,32 +2,33 @@
 print_help() {
 	echo "usage: `basename $PKGMK_COMMAND` [$PKGMK_PKGFILE_NAME] [options]"
 	echo "options:"
-	echo "  -i,   --install             build and install package"
-	echo "  -in,  --interactive         ask for use(s) when possible"
-	echo "  -u,   --upgrade             build and install package (as upgrade)"
-	echo "  -r,   --recursive           search for and build packages recursively"
-	echo "  -d,   --download            download missing source file(s)"
-	echo "  -do,  --download-only       do not build, only download missing source file(s)"
-	echo "  -eo,  --extract-only        do not build, only extract source file(s)"
-	echo "  -utd, --up-to-date          do not build, only check if package is up to date"
-	echo "  -uf,  --update-footprint    update footprint using result from last build"
-	echo "  -if,  --ignore-footprint    build package without checking footprint"
-	echo "  -in,  --ignore-new          build package, ignore new files in a footprint missmatch"
-	echo "  -um,  --update-md5sum       update md5sum"
-	echo "  -im,  --ignore-md5sum       build package without checking md5sum"
-	echo "  -cm,  --check-md5sum        do not build, only check md5sum"
-	echo "  -us,  --update-sha256sum    update sha256sum"
-	echo "  -is,  --ignore-sha256sum    build package without checking sha256sum"
-	echo "  -cs,  --check-sha256sum     do not build, only check sha256sum"
-	echo "  -ns,  --no-strip            do not strip executable binaries or libraries"
-	echo "  -f,   --force               build package even if it appears to be up to date"
-	echo "  -c,   --clean               remove package and downloaded files"
-	echo "  -cp   --check-pkgfile       do not build, only check \`$PKGMK_PKGFILE_NAME'"
-	echo "  -kw,  --keep-work           keep temporary working directory"
-	echo "  -cf,  --config-file <file>  use alternative configuration file"
-	echo "  -li,  --list-includes       list all inclusion files available"
-	echo "  -v,   --version             print version and exit "
-	echo "  -h,   --help                print help and exit"
+	echo "  -i,   --install               build and install package"
+	echo "  -in,  --interactive           ask for use(s) when possible"
+	echo "  -u,   --upgrade               build and install package (as upgrade)"
+	echo "  -r,   --recursive             search for and build packages recursively"
+	echo "  -d,   --download              download missing source file(s)"
+	echo "  -do,  --download-only         do not build, only download missing source file(s)"
+	echo "  -eo,  --extract-only          do not build, only extract source file(s)"
+	echo "  -utd, --up-to-date            do not build, only check if package is up to date"
+	echo "  -uf,  --update-footprint      update footprint using result from last build"
+	echo "  -if,  --ignore-footprint      build package without checking footprint"
+	echo "  -in,  --ignore-new            build package, ignore new files in a footprint missmatch"
+	echo "  -um,  --update-md5sum         update md5sum"
+	echo "  -im,  --ignore-md5sum         build package without checking md5sum"
+	echo "  -cm,  --check-md5sum          do not build, only check md5sum"
+	echo "  -us,  --update-sha256sum      update sha256sum"
+	echo "  -is,  --ignore-sha256sum      build package without checking sha256sum"
+	echo "  -cs,  --check-sha256sum       do not build, only check sha256sum"
+	echo "  -ns,  --no-strip              do not strip executable binaries or libraries"
+	echo "  -f,   --force                 build package even if it appears to be up to date"
+	echo "  -c,   --clean                 remove package and downloaded files"
+	echo "  -cp   --check-pkgfile         do not build, only check \`$PKGMK_PKGFILE_NAME'"
+	echo "  -kw,  --keep-work             keep temporary working directory"
+	echo "  -cf,  --config-file <file>    use alternative configuration file"
+	echo "  -li,  --list-includes         list all inclusion files available"
+	echo "  -pm,  --package-manager <pm>  set the package manager to use"
+	echo "  -v,   --version               print version and exit "
+	echo "  -h,   --help                  print help and exit"
 }
 
 parse_options() {
@@ -88,6 +89,13 @@ parse_options() {
 				shift ;;
 			-li|--list-includes)
 				PKGMK_LIST_INCLUDES="yes";;
+			-pm|--package-manager)
+				if [[ ! "$2" ]]; then
+					echo "`basename $PKGMK_COMMAND`: option $1 requires an argument"
+					exit E_GENERAL
+				fi
+				PKGMK_PACKAGE_MANAGER="$2"
+				shift ;;
 			-v|--version)
 				echo "`basename $PKGMK_COMMAND` $PKGMK_VERSION"
 				exit 0 ;;
