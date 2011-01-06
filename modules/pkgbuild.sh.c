b2aadaf573cc15de2acdebaaab64099cce75bd07
Lukc <lukc@upyum.com>
2011-01-06 22:03:52 +0100

    PKGBUILDs support added, the same way FrugalBuilds are.

---
+++ modules/pkgbuild.sh.c
@@ -0,0 +1,12 @@
+
+/* 
+ * Module to build packages with Arch’s PKGBUILDs. See frugalbuild.sh.c
+ * for the first try of this sort of strange mix. It is basically the 
+ * same thing, except helpers functions are not declared.
+ */
+if [[ "$PKGMK_PKGFILE" = PKGBUILD ]]; then
+	readonly includes=(pkgbuild)
+	
+	PKGMK_NOFAIL="yes"
+fi
+
