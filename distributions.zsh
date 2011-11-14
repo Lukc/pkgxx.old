1ed10b5b9a8f7388ef2f71e1e7a5c19864922fb1
Lukc <lukc@upyum.com>
2011-11-14 18:54:42 +0100

    distributions.zsh update.   * Distributions list updated/completed.   * Tests to know we are on Debian/Ubuntu or any lsb-compliant     distribution have been made.   * Distribution families do now work.

--- distributions.zsh
+++ distributions.zsh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 
 checking_for_bin () {
+	echo -n "checking for $1... "
 	for i in `echo "$PATH" | sed "s|:| |g"`; do
 		if [[ -x $i/$1 ]]; then
 			echo "yes"
@@ -13,7 +14,11 @@ checking_for_bin () {
 
 DISTRIBUTIONS=(
 	# We put here only the most known distributions.
-	Crux Frugalware Fedora Mandriva Arch Slackware Debian FreeBSD
+	Crux Arch Slackware Frugalware   # KISS
+	Fedora Mandriva                  # RPM-based
+	Debian Ubuntu                    # dpkg/Debian-based
+	FreeBSD OpenBSD NetBSD           # *BSD
+	Cygwin                           # Problems.
 )
 
 if [[ -e /usr/bin/crux ]]; then
@@ -28,24 +33,38 @@ elif [[ -e /etc/arch-release ]]; then
 	: ${DISTRIBUTION:="Arch"}
 elif [[ $PACKAGE_MANAGER = pkgtools ]]; then
 	: ${DISTRIBUTION:="Slackware"}
-elif [[ $PACKAGE_MANAGER = dpkg ]] && checking_for_bin "apt-get";
-then
-	# FIXME: Debian is not the only distribution with dpkg…
-	: ${DISTRIBUTION:="Debian"}
-elif [[ "$KERNEL" = FreeBSD ]]; then
-	: ${DISTRIBUTION:="FreeBSD"}
+elif [[ -e /etc/lsb-release ]]; then
+	if . /etc/lsb-release; then
+		# Should cover Ubuntu and Debian, at least.
+		: ${DISTRIBUTION:="$DISTRIB_ID"}
+		: ${DISTRIBUTION_VERSION:="$DISTRIB_RELEASE"}
+	else
+		echo "  /etc/lsb-release could not be parsed." >&2
+	fi
+elif [[ "$KERNEL" =~ (Free|Net|Open)BSD ]]; then
+	# Both should be valid.
+	: ${DISTRIBUTION:="$KERNEL"}
 else
 	: ${DISTRIBUTION:="Unknown"}
 fi
 
+case $DISTRIBUTION in
+	Ubuntu)   FAMILY=Debian ;;
+	*)        FAMILY=$DISTRIBUTION ;;
+esac
+
+[[ -n "$DISTRIBUTION" ]] && echo "distribution=\"$DISTRIBUTION\""
+[[ -n "$FAMILY" ]]       && echo "distribution_family=\"$FAMILY\""
 
 for d in ${DISTRIBUTIONS[@]}; do
 	if [[ "$DISTRIBUTION" = "$d" ]]; then
-		echo "distribution=\"$DISTRIBUTION\""
-		echo "distribution_family=\"$DISTRIBUTION\""
 		echo "$d=true"
 	else
-		echo "$d=false"
+		if [[ "$FAMILY" = "$d" ]]; then
+			echo "$d=true"
+		else
+			echo "$d=false"
+		fi
 	fi
 done
 
