2b98a9edce0953536f8add75b3821626f56151fe
Lukc <lukc@upyum.com>
2011-11-25 21:23:03 +0100

    Revisionning added for hg.

--- modules/hg.zsh
+++ modules/hg.zsh
@@ -3,6 +3,11 @@ hg:clone() {
 	hg clone `echo $1 | sed -e 's|^hg+||'` $name
 }
 
+hg:revision() {
+	cd "$1"
+	echo $(hg tip | head -n 1 | cut -d : -f 2)
+}
+
 hg:pull() {
 	if [[ "`hg pull | tail -n 1`" = "no changes found" ]]; then
 		return 1
