b6ea7f2468e6a00c1d56628627203176650179f9
Lukc <lukc@upyum.com>
2012-01-18 13:40:23 +0100

    Minor modifications of distributions.zsh.   * Gentoo added to the list of known distributions, even tough it does     not have easy binary package management.   * The <distribution>_version variable is now set, when the data is     available.

--- distributions.zsh
+++ distributions.zsh
@@ -19,6 +19,9 @@ DISTRIBUTIONS=(
 	Debian Ubuntu                    # dpkg/Debian-based
 	FreeBSD OpenBSD NetBSD           # *BSD
 	Cygwin                           # Problems.
+	Gentoo                           # Also problems, as those distro do
+	                                 #+ not use any kind of native binary
+					 #+ package management.
 )
 
 if [[ -e /usr/bin/crux ]]; then
@@ -31,8 +34,9 @@ elif [[ -e /etc/mandriva-release ]]; then
 	: ${DISTRIBUTION:="Mandriva"}
 elif [[ -e /etc/arch-release ]]; then
 	: ${DISTRIBUTION:="Arch"}
-elif [[ $PACKAGE_MANAGER = pkgtools ]]; then
-	: ${DISTRIBUTION:="Slackware"}
+elif [[ -e /etc/gentoo-release ]]; then
+	: ${DISTRIBUTION:="Gentoo"}
+	: ${DISTRIBUTION_VERSION:="$(cat /etc/gentoo-release | sed -e 's/.*release //')"}
 elif [[ -e /etc/lsb-release ]]; then
 	if . /etc/lsb-release; then
 		# Should cover Ubuntu and Debian, at least.
@@ -41,6 +45,8 @@ elif [[ -e /etc/lsb-release ]]; then
 	else
 		echo "  /etc/lsb-release could not be parsed." >&2
 	fi
+elif [[ $PACKAGE_MANAGER = pkgtools ]]; then
+	: ${DISTRIBUTION:="Slackware"}
 elif [[ "$KERNEL" =~ (Free|Net|Open)BSD ]]; then
 	# Both should be valid.
 	: ${DISTRIBUTION:="$KERNEL"}
@@ -53,12 +59,14 @@ case $DISTRIBUTION in
 	*)        FAMILY=$DISTRIBUTION ;;
 esac
 
-[[ -n "$DISTRIBUTION" ]] && echo "distribution=\"$DISTRIBUTION\""
-[[ -n "$FAMILY" ]]       && echo "distribution_family=\"$FAMILY\""
+[[ -n "$DISTRIBUTION" ]]         && echo "distribution=\"$DISTRIBUTION\""
+[[ -n "$FAMILY" ]]               && echo "distribution_family=\"$FAMILY\""
+[[ -n "$DISTRIBUTION_VERSION" ]] && echo "distribution_version=\"$DISTRIBUTION_VERSION\""
 
 for d in ${DISTRIBUTIONS[@]}; do
 	if [[ "$DISTRIBUTION" = "$d" ]]; then
 		echo "$d=true"
+		echo "${d}_version=$DISTRIBUTION_VERSION"
 	else
 		if [[ "$FAMILY" = "$d" ]]; then
 			echo "$d=true"
