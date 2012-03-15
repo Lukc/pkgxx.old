f87c11536e61434aff8033fee1e5e869590f09ff
Lukc <lukc@upyum.com>
2012-03-15 08:17:22 +0100

    modules/debian.zsh completed.   * Debian:headers and Debian:documentation have been added.   * Documentation of Debian:library has also been corrected.

--- modules/debian.zsh
+++ modules/debian.zsh
@@ -12,12 +12,32 @@ Debian:library() {
 	# 
 	# Adds `lib` at the begining of `$pkgname`, if it does not already
 	# begins by the same string.
-	#
-	#   # Exemples
+	# 
+	#   # Examples
 	#   tre    -> libtre
 	#   libsdl -> libsdl
 	#>
 	pkgname="lib${pkgname}"
 	pkgname="${pkgname/^liblib/lib}"
+}
+
+Debian:documentation() {
+	#<
+	# ## `Debian:documentation`
+	# 
+	# Adds `-doc` at the end of `$pkgname` and removes duplicate `-doc`.
+	#>
+	pkgname="${pkgname}-doc"
+	pkgname="${pkgname/-doc-doc$/-doc}"
+}
+
+Debian:headers() {
+	#<
+	# ## `Debian:headers`
+	# 
+	# Adds `-dev` at the end of `$pkgname` and removes duplicate `-dev`.
+	#>
+	pkgname="${pkgname}-dev"
+	pkgname="${pkgname/-dev-dev$/-dev}"
 }
 
