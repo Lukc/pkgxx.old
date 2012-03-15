c0e71e5dcd8fa162f14766c1a838fc4508fb7004
Lukc <lukc@upyum.com>
2012-03-15 09:17:00 +0100

    Buggy code removal.   * I suppose this was here to fix problems with dpkg or     something like that. The correction should then be in     dpkg:build() and concerning pkgname, not in main() and     modifying name.

--- src/main.sh.c
+++ src/main.sh.c
@@ -146,14 +146,6 @@ main() {
 	fi
 	
 	/* 
-	 * If ${name} contains '-', there will be problems later in build().
-	 * So, we just remove those '-'.
-	 */
-	if [[ "$name" =~ .*-.* ]]; then
-		name="${name//-/_}"
-	fi
-	
-	/* 
 	 * Once the recipes are parsed and pkgname is exported, post-recipe
 	 * defaults are read.
 	 */
