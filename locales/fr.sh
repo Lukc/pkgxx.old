0480005271e531fed507ba476753590720112ba6
Pingax <anthony@pingax.no-ip.info>
2012-04-28 00:45:20 +0200

    locales/fr.sh * French translation (1rt import)

---
+++ locales/fr.sh
@@ -0,0 +1,15 @@
+
+msg_md5sum_updated="Somme Md5 mise à jour."
+msg_sha256sum_updated="Somme Sha256 mise à jour."
+
+msg_extracting_sources="Extraction des sources du paquet \`%1'."
+
+msg_not_up_to_date="Le paquet \`%1' n'est pas à jour."
+msg_up_to_date="Le paquet \`%1' est à jour."
+
+msg_unavailable_profile="Requested profile (%1) is not available."
+msg_file_not_found="Le fichier \`%1' n'a pas été trouvé."
+msg_no_recipe_format="Aucune possibilité d'analyse, aucune recette trouvée. Veuillez installer un module."
+msg_cannot_be_parsed="\`%1' ne peut pas être analysé"
+msg_missing_dependencies="Toutes les dépendances n'ont pas été trouvées sur votre système."
+
