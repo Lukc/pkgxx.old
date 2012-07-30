91ed423e5cfdf48841dc60edab4c8b4b74c2a5ca
Pingax <anthony@pingax.no-ip.info>
2012-07-30 13:24:33 +0200

    * Update of french translation (WIP)

--- locales/fr.sh
+++ locales/fr.sh
@@ -1,4 +1,8 @@
 
+# Common
+msg_func_may_have_failed="%1() may have failed."
+
+# main.sh.c
 msg_md5sum_updated="Somme Md5 mise à jour."
 msg_sha256sum_updated="Somme Sha256 mise à jour."
 
@@ -8,8 +12,25 @@ msg_not_up_to_date="Le paquet \`%1' n'est pas à jour."
 msg_up_to_date="Le paquet \`%1' est à jour."
 
 msg_unavailable_profile="Requested profile (%1) is not available."
-msg_file_not_found="Le fichier \`%1' n'a pas été trouvé."
-msg_no_recipe_format="Aucune possibilité d'analyse, aucune recette trouvée. Veuillez installer un module."
-msg_cannot_be_parsed="\`%1' ne peut pas être analysé"
+msg_file_not_found="Fichier \`%1' non trouvé."
+msg_no_recipe_format="No way to parse any recipe has been found. Please install a recipe module."
+msg_cannot_be_parsed="\`%1' ne peut être parsé."
 msg_missing_dependencies="Toutes les dépendances n'ont pas été trouvées sur votre système."
+
+# build.sh.c
+msg_should_build_as_root="Les paquets devraient être construits en tant que super-utilisateur."
+
+msg_building_package="Compilation \`%1'%2."
+msg_build_success="Compilation \`%1' effectuée avec succès."
+msg_build_fail="Construction \`%1' échouée."
+
+msg_testing="Test de \`%1'."
+msg_tests_success="\`%1' a été correctement testé."
+msg_tests_fail="Les tests de \`%1' ont échoué."
+
+msg_log_avail_at="Journal disponible ici : %1"
+
+msg_bad_autosplit="Automatic split \`%1' is not available."
+
+msg_build_result="Build result:"
 
