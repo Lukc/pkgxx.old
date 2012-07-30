a0630d9fe787a73c733f3155c92b1e8cc8b9ca04
pingax <pingax@debian.(none)>
2012-07-30 13:32:31 +0200

    * End of french translation

--- locales/fr.sh
+++ locales/fr.sh
@@ -1,6 +1,6 @@
 
 # Common
-msg_func_may_have_failed="%1() may have failed."
+msg_func_may_have_failed="%1() a échoué."
 
 # main.sh.c
 msg_md5sum_updated="Somme Md5 mise à jour."
@@ -11,26 +11,26 @@ msg_extracting_sources="Extraction des sources du paquet \`%1'."
 msg_not_up_to_date="Le paquet \`%1' n'est pas à jour."
 msg_up_to_date="Le paquet \`%1' est à jour."
 
-msg_unavailable_profile="Requested profile (%1) is not available."
+msg_unavailable_profile="Le profil demandé (%1) n'est pas disponible."
 msg_file_not_found="Fichier \`%1' non trouvé."
-msg_no_recipe_format="No way to parse any recipe has been found. Please install a recipe module."
-msg_cannot_be_parsed="\`%1' ne peut être parsé."
-msg_missing_dependencies="Toutes les dépendances n'ont pas été trouvées sur votre système."
+msg_no_recipe_format="Aucune méthode pour analyser le fichier recette n'a été trouvée. Veuillez installer au moins un module."
+msg_cannot_be_parsed="\`%1' ne peut être analysé."
+msg_missing_dependencies="Toutes les dépendances requises n'ont pas été trouvées sur votre système."
 
 # build.sh.c
 msg_should_build_as_root="Les paquets devraient être construits en tant que super-utilisateur."
 
-msg_building_package="Compilation \`%1'%2."
-msg_build_success="Compilation \`%1' effectuée avec succès."
-msg_build_fail="Construction \`%1' échouée."
+msg_building_package="Construction de \`%1'%2."
+msg_build_success="Construction de \`%1' effectuée avec succès."
+msg_build_fail="Construction de \`%1' échouée."
 
 msg_testing="Test de \`%1'."
-msg_tests_success="\`%1' a été correctement testé."
+msg_tests_success="Les tests de \`%1' se sont correctement déroulés."
 msg_tests_fail="Les tests de \`%1' ont échoué."
 
 msg_log_avail_at="Journal disponible ici : %1"
 
-msg_bad_autosplit="Automatic split \`%1' is not available."
+msg_bad_autosplit="Le split automatique de \`%1' n'est pas disponible."
 
-msg_build_result="Build result:"
+msg_build_result="Résultat de la construction :"
 
